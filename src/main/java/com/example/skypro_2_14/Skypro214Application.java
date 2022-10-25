171f4c51723c7bd8ef55c84480bb46709f7efe4b
Olga Kargan <olgakargan72@gmail.com>
2022-10-25 17:43:46 +0400

    первый коммит

---
+++ src/main/java/com/example/skypro_2_14/Skypro214Application.java
@@ -0,0 +1,13 @@
+package com.example.skypro_2_14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Skypro214Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Skypro214Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/skypro_2_14/Skypro214ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skypro_2_14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Skypro214ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
